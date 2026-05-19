9caf94f2f2250f96e2d029602b6041c9af427903
SoftwareDev Stan <mochogestanley80@gmail.com>
2026-05-19 14:43:10 +0300

    Stripe/Express/Server

--- UCLanServerDB/server.js
+++ UCLanServerDB/server.js
@@ -19,8 +19,8 @@ app.use('/api/stripe', express.raw({type: 'application/json'}), stripeWebhook)
 
 
 // Middleware
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 app.use(clerkMiddleware());
 
 await connectDB();
